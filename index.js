cc944fd3f2241ac98a460202194f858d5df22a1c
Alex <alex.rawhti@hotmail.com>
2025-10-29 10:08:31 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import { loadHighScores, ensureCanvas, gameLoop } from './game.js';
+import { respawnPlayer, respawnGoldStar } from './utils.js';
+import { spawnWave } from './waveManager.js';
+import { setupInputHandlers } from './input.js';
 
+window.addEventListener("load", () => {
+  loadHighScores();
+  ensureCanvas();
+  respawnPlayer();
+  respawnGoldStar();
+  spawnWave(0);
+  setupInputHandlers();
+  requestAnimationFrame(gameLoop);
+});
